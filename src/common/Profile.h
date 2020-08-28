7db5f08006463040e60712a091b05cfb2cfb1cf2
slipher <slipher@protonmail.com>
2020-08-28 16:32:44 -0700

    Add more PROF* macros

--- src/common/Profile.h
+++ src/common/Profile.h
@@ -2,6 +2,7 @@
 #define COMMON_PROFILE_H_
 
 #include "Compiler.h"
+#include "Util.h"
 
 struct prof {
     const char* name;
@@ -12,5 +13,8 @@ struct prof {
 };
 
 #define PROF(name, ...) { prof _prof_##__COUNTER__(#name); __VA_ARGS__; }
+#define PROFC(name, cond, ...) { auto _prof_##__COUNTER__ = (cond) ? Util::optional<prof>(#name) : Util::nullopt; __VA_ARGS__; }
+#define PROFB(name) prof _prof_##__COUNTER__(#name);
+#define PROFBC(name, cond) auto _prof_##__COUNTER__ = (cond) ? Util::optional<prof>(#name) : Util::nullopt;
 
 #endif // COMMON_PROFILE_H_
