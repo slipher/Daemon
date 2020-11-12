d7365a6acd9da843c8df8ed33e415838015f3468
slipher <slipher@protonmail.com>
2020-11-12 15:45:01 -0600

    REVERT externaldeps testing scripts

---
+++ external_deps/mw32.sh
@@ -0,0 +1,6 @@
+d=$(dirname $(realpath $0))
+touch mingw32.log
+for a in zlib gmp nettle geoip curl sdl2 glew png jpeg webp freetype openal ogg vorbis speex opus opusfile lua naclsdk naclports; do
+    $d/build.sh mingw32 $a
+    echo $a $? >> mingw32.log
+done
---
+++ external_deps/mw64.sh
@@ -0,0 +1,7 @@
+d=$(dirname $(realpath $0))
+rm -rf build-mw64-5/ mingw64.log
+touch mingw64.log
+for a in zlib gmp nettle geoip curl sdl2 glew png jpeg webp freetype openal ogg vorbis speex opus opusfile lua naclsdk naclports; do
+    $d/build.sh mingw64 $a
+    echo $a $? >> mingw64.log
+done
