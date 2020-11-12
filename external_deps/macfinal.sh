10dba68b3f433c13db2fb648d818f11d1e85fae3
slipher <slipher@protonmail.com>
2020-11-12 15:45:01 -0600

    REVERT externaldeps build testing scripts

---
+++ external_deps/macfinal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+brew install automake libtool cmake
+d=$(dirname $0)
+$d/build.sh macosx64 clean || exit
+$d/build.sh macosx64 pkgconfig nasm gmp nettle geoip sdl2 glew png jpeg webp freetype openal ogg vorbis speex opus opusfile lua naclsdk naclports install package
