5d9785a318f025f0f5e53fcf03886a9a93e7521a
slipher <slipher@protonmail.com>
2020-11-17 05:29:08 -0600

    build test scripts

--- external_deps/wbuild.sh
+++ external_deps/wbuild.sh
@@ -1,6 +1,9 @@
+if [ "${#}" -lt "1" ]; then
+    echo "usage: ${0} <platform>"
+fi
 d=$(dirname $(realpath $0))
-rm -rf build-mw64-5/ mingw64.log
+rm -rf build-$1-5/ $1.log
 for a in zlib gmp nettle geoip curl sdl2 glew png jpeg webp freetype openal ogg vorbis speex opus opusfile lua naclsdk naclports; do
-    $d/build.sh mingw64 $a
-    echo $a $? >> mingw64.log
+    $d/build.sh $1 $a
+    echo $a $? >> $1.log
 done
