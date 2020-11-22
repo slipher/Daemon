2402e1b54dd4dcc517b73484856b912755bd78ac
slipher <slipher@protonmail.com>
2020-11-22 12:17:56 -0600

    build test script

--- external_deps/wbuild.sh
+++ external_deps/wbuild.sh
@@ -1,9 +1,11 @@
-if [ "${#}" -lt "1" ]; then
+if [ "${#}" -ne "1" ]; then
     echo "usage: ${0} <platform>"
+    exit
 fi
 d=$(dirname $(realpath $0))
-rm -rf build-$1-5/ $1.log
-for a in zlib gmp nettle geoip curl sdl2 glew png jpeg webp freetype openal ogg vorbis speex opus opusfile lua naclsdk naclports; do
+$d/build.sh $1 clean || exit
+rm -f $1.log
+for a in zlib gmp nettle geoip curl sdl2 glew png jpeg webp freetype openal ogg vorbis speex opus opusfile lua naclsdk naclports gendef; do
     $d/build.sh $1 $a
     echo $a $? >> $1.log
 done
